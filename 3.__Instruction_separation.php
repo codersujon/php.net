a3d9c7f2531da365ea6e73905ed23e0a60f55db6
MD.SUJAN AHMED <csesujon155@gmail.com>
2023-09-07 02:33:24 +0600

    Instruction Separation

---
+++ 3.__Instruction_separation.php
@@ -0,0 +1,15 @@
+<!-- Instruction Separation -->
+<?php echo "I am Okay"; ?>
+<p>No newline</p>
+<?= "Say Okay" ?>
+
+<?php
+
+    /**
+     * *write more than one statement in one line, just separating with a semicolon
+     */
+
+    echo "a"; echo "b"; echo "c";
+
+
+
